09477167f275b900b3b7acce82fb6402ac7ea476
unknown <minkhantthawhtan11@gmail.com>
2025-11-30 11:55:15 +0630

    add new3.py

---
+++ new3.py
@@ -0,0 +1,22 @@
+# name = "mg mg"
+# age = 24
+# print ( f"{name } is  {age} years old.")
+    
+# name = input ("what is your name? \n")
+# age = input("How old are you?\n")
+# print("---------------------------------")
+# print ( f"{name } is  {age} years old.")
+
+num1 = input("Enter Number1 \n")
+num2 = input("Enter Number2 \n")
+
+add = int(num1)+ int(num2)
+sub = int(num1)-int(num2)
+multi = int(num1)*int(num2)
+div = int(num1)/int (num2)
+print(f"Your answer is {total}.")
+
+print(f"Your addition answer is {Add}")
+print(f"Your subtraction answer is {sub}")
+print(f"Your multi answer is {multi}")
+print(f"Your Division answer is {div}")
